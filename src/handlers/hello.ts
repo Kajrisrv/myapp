f9342ba7b60fa0eb857fd275daa732266f6da6f6
Kajrisrv <kajrisrv@gmail.com>
2020-09-09 20:48:57 +0530

    second

---
+++ src/handlers/hello.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyHandler, APIGatewayProxyResult, APIGatewayProxyEvent, Context } from "aws-lambda";
+
+export const helloHandler: APIGatewayProxyHandler = async (event:APIGatewayProxyEvent, context:Context) => {
+    let response:APIGatewayProxyResult;
+    response = {
+        body: "Hello World!",
+        statusCode: 200
+    }
+
+    return response;
+};
